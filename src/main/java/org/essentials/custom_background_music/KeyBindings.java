1e5618f3da5683c41489abbd80cc7f16202b1d6f
DashKetch <nateisthegreatgreat@gmail.com>
2025-11-09 18:22:19 -0800

    changes and additions

--- src/main/java/org/essentials/custom_background_music/KeyBindings.java
+++ src/main/java/org/essentials/custom_background_music/KeyBindings.java
@@ -1,15 +1,18 @@
-// ========== KeyBindings.java ==========
 package org.essentials.custom_background_music;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.client.settings.KeyConflictContext;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyBindings {
     public static final KeyMapping OPEN_MUSIC_GUI = new KeyMapping(
             "key.custom_background_music.open_gui",
+            KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_M,
-            KeyMapping.Category.MISC
+            "key.categories.custom_background_music"
     );
 
     public static void register(RegisterKeyMappingsEvent event) {
